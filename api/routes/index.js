0def4a259431bd7aac2c342b3c3d01cf86a81a35
isaachintosh <isaac.tuning@gmail.com>
2021-08-10 11:45:55 -0300

    created first route

---
+++ api/routes/index.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser')
+const { Routes } = require('express')
+
+module.exports = app => {
+    app.use(bodyParser.json())
+    app.get('/', (req, res) => {
+        res.send('Rota foi acessada')
+    })
+}
