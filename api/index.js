9aeef9cff14fe72c00bdcd476f13484349a2cbb8
isaachintosh <isaac.tuning@gmail.com>
2021-08-10 11:40:11 -0300

    initiated routes relocation

--- api/index.js
+++ api/index.js
@@ -7,11 +7,6 @@ app.use(bodyParser.json())
 
 const port = 3000
 
-app.get('/teste', (req, res) => {
-    res.status(200)
-    .send({ mensagem: 'Boas vindas à API!' })
-})
-
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta: ${port}`)
 })
