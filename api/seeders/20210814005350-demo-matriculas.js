77a9082f6f2414eda7ad3ea037ceb907d8aaa112
isaachintosh <isaac.tuning@gmail.com>
2021-08-14 00:21:36 -0300

    data examples has been add to MATRICULAS seeder

--- api/seeders/20210814005350-demo-matriculas.js
+++ api/seeders/20210814005350-demo-matriculas.js
@@ -42,6 +42,20 @@ module.exports = {
         turma_id: 2,
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        status: "confirmado",
+        estudante_id: 5,
+        turma_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "cancelado",
+        estudante_id: 6,
+        turma_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
