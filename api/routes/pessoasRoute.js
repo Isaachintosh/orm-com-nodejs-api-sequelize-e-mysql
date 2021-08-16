d200373a6bba59eb2ea594492206a2a5c2ac56a3
isaachintosh <isaac.tuning@gmail.com>
2021-08-16 13:51:33 -0300

    included route addess to pegaUmaMatricula method

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,14 +3,12 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
-
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-
-router.post('/pessoas', PessoaController.criarPessoa)
-
-router.put('/pessoas/:id', PessoaController.atualizaPessoa)
-
-router.delete('/pessoas/:id', PessoaController.deletaPessoa)
+router
+    .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+    .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+    .post('/pessoas', PessoaController.criarPessoa)
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .delete('/pessoas/:id', PessoaController.deletaPessoa)
+    .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
 
 module.exports = router
